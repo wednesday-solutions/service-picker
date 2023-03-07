a78e82b57e7d8937e0c1a2ea9b48b8a2f4588477
Ijas Mohamad <ijas@wednesday.is>
2023-03-07 10:06:31 +0530

    fix: change error logger

--- utils/errorhandler/error.go
+++ utils/errorhandler/error.go
@@ -1,8 +1,10 @@
 package errorhandler
 
+import "log"
+
 func CheckNilErr(err error) {
 	if err != nil {
-		// panic(err) will stop execution and throw error
-		panic(err)
+		// log.Fatal(err) will stop execution and throw error
+		log.Fatal(err)
 	}
 }
