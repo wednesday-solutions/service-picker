d9b87d101f29bfc98992233a0555d9d7140982c8
Ijas Mohamad <ijas@wednesday.is>
2023-03-14 19:40:54 +0530

    fix: update package json through terminal

--- utils/helpers/updatePackageDotJson.go
+++ utils/helpers/updatePackageDotJson.go
@@ -2,42 +2,38 @@ package helpers
 
 import (
 	"fmt"
-	"io/ioutil"
-	"strings"
+	"log"
+	"os/exec"
 
-	"github.com/wednesday-solutions/picky/utils/constants"
 	"github.com/wednesday-solutions/picky/utils/errorhandler"
 	"github.com/wednesday-solutions/picky/utils/fileutils"
 )
 
-func UpdatePackageDotJson(stack, jsonFile, database, projectName string) error {
+func UpdatePackageDotJson(stack, database string) error {
 
-	jsonFile = fmt.Sprintf("%s%s", fileutils.CurrentDirectory(), jsonFile)
+	fmt.Println("Loading... (please wait, this will take some time)")
 
-	input, err := ioutil.ReadFile(jsonFile)
-	errorhandler.CheckNilErr(err)
-
-	lines := strings.Split(string(input), "\n")
-	var updatePackageDotJson bool
-
-	switch stack {
-	case constants.NODE_HAPI:
-
-		if database == constants.POSTGRES {
-			updatePackageDotJson = true
-			lines[43] = fmt.Sprintf("%s\n\t\t%s", lines[43], `"cls-hooked": "4.2.2",`)
-			lines[62] = fmt.Sprintf("%s\n\t\t%s", lines[62], `"pg": "^8.10.0",`)
-			lines[63] = fmt.Sprintf("%s\n\t\t%s", lines[63], `"pg-native": "3.0.1",`)
-			lines[66] = fmt.Sprintf("\t\t%s", `"sequelize": "^6.6.5",`)
-			lines[107] = fmt.Sprintf("\t\t%s", `"sequelize-cli": "^6.6.0",`)
+	var command string
+	updateCommands := []string{}
+	err := exec.Command("yarn", "-v").Run()
+	if err != nil {
+		log.Fatal("Command ", err)
+		err = exec.Command("npm", "-v").Run()
+		if err != nil {
+			log.Fatal("Please install 'yarn' or 'npm' in your machine.")
+		} else {
+			command = "npm"
+			updateCommands = append(updateCommands, "install", "--legacy-peer-deps", "--save", "pg", "pg-native")
 		}
+	} else {
+		command = "yarn"
+		updateCommands = append(updateCommands, "add", "pg", "pg-native")
 	}
-	output := strings.Join(lines, "\n")
 
-	if updatePackageDotJson {
-		err := fileutils.WriteToFile(jsonFile, output)
-		errorhandler.CheckNilErr(err)
-	}
+	cmd := exec.Command(command, updateCommands...)
+	cmd.Dir = fmt.Sprintf("%s/%s", fileutils.CurrentDirectory(), "backend")
+	err = cmd.Run()
+	errorhandler.CheckNilErr(err)
 
 	return nil
 }
