1e737706e4fab85a47634d60cdc4ce5ed65ce50d
Ijas Mohamad <ijas@wednesday.is>
2023-05-11 16:27:10 +0530

    fix(multiple-service): Fix multiple service selection
    If user try to select multiple same service cli will show a message and will redirect to home

--- prompt/service.go
+++ prompt/service.go
@@ -2,6 +2,8 @@ package prompt
 
 import (
 	"github.com/wednesday-solutions/picky/internal/constants"
+	"github.com/wednesday-solutions/picky/internal/errorhandler"
+	"github.com/wednesday-solutions/picky/internal/utils"
 )
 
 var services = []string{
@@ -26,9 +28,33 @@ func PromptSelectService() {
 func (i *InitInfo) PromptSelectStack() {
 
 	i.Stack = i.PromptStack()
+	_ = DisplayMultipleStackWarningMessage(i.Service)
 	if i.Service == constants.Backend {
 		i.PromptSelectStackDatabase()
 	}
+	if DisplayMultipleStackWarningMessage(i.Service) {
+		PromptHome()
+	}
 	i.DirName = i.PromptGetDirectoryName()
 	i.PromptSelectInit()
+}
+
+func DisplayMultipleStackWarningMessage(service string) bool {
+	var serviceExist bool
+	backendExist, webExist, _ := utils.IsBackendWebAndMobileExist()
+	var err error
+	if service == constants.Web {
+		if webExist {
+			err = utils.PrintWarningMessage("We are working on supporting multiple frontend stacks in the upcoming releases!")
+			errorhandler.CheckNilErr(err)
+			serviceExist = true
+		}
+	} else if service == constants.Backend {
+		if backendExist {
+			err = utils.PrintWarningMessage("We are working on supporting multiple backend stacks in the upcoming releases!")
+			errorhandler.CheckNilErr(err)
+			serviceExist = true
+		}
+	}
+	return serviceExist
 }
