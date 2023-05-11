ad72d0020008b02cbacb6ad3410c7ae06701c0bf
Ijas Mohamad <ijas@wednesday.is>
2023-05-11 19:55:05 +0530

    fix(worflow-branch)

--- cmd/test.go
+++ cmd/test.go
@@ -1,42 +1,41 @@
 package cmd
 
 import (
+	"fmt"
+
+	"github.com/rivo/tview"
 	"github.com/spf13/cobra"
 	"github.com/wednesday-solutions/picky/internal/errorhandler"
-	"github.com/wednesday-solutions/picky/internal/utils"
 )
 
 var TestCommand = TestCommandFn()
 
 func RunTest(*cobra.Command, []string) error {
 
-	// var firstName string
-	// var region string
+	var firstName string
+	var region string
 
-	// app := tview.NewApplication()
-	// form := tview.NewForm()
-	// form.AddInputField("Project Infra Name", "", 20, nil, func(text string) {
-	// 	firstName = text
-	// })
-	// form.AddDropDown("Select Infra Region", []string{"india", "usa"}, 0, func(option string, optionIndex int) {
-	// 	region = option
-	// })
+	app := tview.NewApplication()
+	form := tview.NewForm()
+	form.AddInputField("Project Infra Name", "", 20, nil, func(text string) {
+		firstName = text
+	})
+	form.AddDropDown("Select Infra Region", []string{"india", "usa"}, 0, func(option string, optionIndex int) {
+		region = option
+	})
 
-	// form.AddButton("Save", func() {
-	// 	app.Stop()
-	// 	fmt.Println("First Name: ", firstName, "\nInfra Region: ", region)
-	// })
-	// form.AddButton("Quit", func() {
-	// 	app.Stop()
-	// })
+	form.AddButton("Save", func() {
+		app.Stop()
+		fmt.Println("First Name: ", firstName, "\nInfra Region: ", region)
+	})
+	form.AddButton("Quit", func() {
+		app.Stop()
+	})
 
-	// form.SetBorder(true).SetTitle("Enter infra details").SetTitleAlign(tview.AlignLeft)
-	// if err := app.SetRoot(form, true).EnableMouse(true).Run(); err != nil {
-	// 	errorhandler.CheckNilErr(err)
-	// }
-
-	err := utils.CreateJsonFile()
-	errorhandler.CheckNilErr(err)
+	form.SetBorder(true).SetTitle("Enter infra details").SetTitleAlign(tview.AlignLeft)
+	if err := app.SetRoot(form, true).EnableMouse(true).Run(); err != nil {
+		errorhandler.CheckNilErr(err)
+	}
 
 	return nil
 }
